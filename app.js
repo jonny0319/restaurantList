6ceb07ec247c536ad71210bab57ac56a0335790b
Jonny Chung <jonny0319@hotmail.com>
2023-09-11 16:16:23 +0800

    Enable Bootstrap in project

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Add static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
