e606bfd71759eaf13521838af728ed6b6c814b77
Jonny Chung <jonny0319@hotmail.com>
2023-09-12 10:49:45 +0800

    Add looping to reduce repetitive codes
    Use handlebars looping feature and data from restaurant.json
    to reduce repetivtive codes. The loop will loop through each
    array element from result array of restaurant.json to create
    the full list of restaurants.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: restaurant.results[0] })
+  res.render('index', { restaurant: restaurant.results })
 
 })
 
